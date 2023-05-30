c784fd930f0b16fee3bed80ea83b08668616e1d7
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2023-05-30 10:25:23 +0300

    Python Principles Challange: Anagrams

---
+++ Strings/Anagrams/anagrams.py
@@ -0,0 +1,6 @@
+def is_anagram(str1, str2):
+    return sorted(str1) == sorted(str2)
+
+print(is_anagram('typhoon', 'opython'))
+print(is_anagram('Alice', 'Bob'))
+print(is_anagram('test', 'tess'))
--- Strings/m.py
+++
@@ -1,8 +0,0 @@
-def count(string):
-    return string.count('-') + 1
-
-
-print(count('ho-tel'))
-print(count('cat'))
-print(count('met-a-phor'))
-print(count('ter-min-a-tor'))
