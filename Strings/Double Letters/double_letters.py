ed8f27c8e3ea9b41889738a8009a7f09dd98992b
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2023-05-30 09:48:05 +0300

    Python Principles Challange: Double Letters

--- Strings/Double Letters/double_letters.py
+++ Strings/Double Letters/double_letters.py
@@ -1,10 +1,8 @@
 def double_letters(string):
-    is_double_letters = False
     for i in range(len(string) - 1):
         if string[i] == string[i + 1]:
-            is_double_letters = True
-            break
-    return is_double_letters
+            return True
+    return False
 
 
 print(double_letters('hello'))
