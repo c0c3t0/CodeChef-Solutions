c4c42fb72b4eb3a16b2ddbbf96a0dd831a42475e
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2023-05-30 12:54:16 +0300

    Python Principles Challange: Thousands Separator

---
+++ Nums/Divisible by 3/Thousands separator/thousands_separator.py
@@ -0,0 +1,5 @@
+def format_number(num):
+    return f'{num:,}'
+
+
+print(format_number(1000000))
--- Strings/m.py
+++
@@ -1,7 +0,0 @@
-def param_count(*args):
-    var = args
-    return len(args)
-    
-    
-print(param_count())
-print(param_count(3,4,5))
