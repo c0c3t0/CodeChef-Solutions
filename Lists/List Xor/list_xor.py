5d74df9c2ebae700443909bc093353c481f4dccc
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2023-05-30 12:31:56 +0300

    Python Principles Challange: List Xor

--- Lists/List Xor/list_xor.py
+++ Lists/List Xor/list_xor.py
@@ -4,8 +4,9 @@ def list_xor(n, list1, list2):
     if n not in list1 and n not in list2:
         return False
     return True
-    
+
+
 print(list_xor(1, [1, 2, 3], [4, 5, 6]))
-print(list_xor(1, [0, 2, 3], [1, 5, 6]) )
-print(list_xor(1, [1, 2, 3], [1, 5, 6]) )
+print(list_xor(1, [0, 2, 3], [1, 5, 6]))
+print(list_xor(1, [1, 2, 3], [1, 5, 6]))
 print(list_xor(1, [0, 0, 0], [4, 5, 6]))
